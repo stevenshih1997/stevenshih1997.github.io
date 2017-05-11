c495f6a6e8b399c478f79aaf5d41f13ac933ffcf
Steven Shih <stevenshih1997@gmail.com>
2017-05-10 22:46:52 -0700

    updated fonts

--- scripts/animated-projectname.js
+++ scripts/animated-projectname.js
@@ -6,7 +6,7 @@ var ctx = document.querySelector("canvas").getContext("2d"),
     dashLen = 220, dashOffset = dashLen, speed = 15,
     txt = "Projects", x = 30, i = 0;
 
-ctx.font = "60px Calibri, italic, TSCu_Comic, sans-serif"; 
+ctx.font = "60px Quando, italic, TSCu_Comic, sans-serif"; 
 ctx.lineWidth = 1.5; ctx.lineJoin = "round"; ctx.globalAlpha = 2.5/3;
 ctx.strokeStyle = ctx.fillStyle = "#ffffff";
 
